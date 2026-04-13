4b6966e315c06fb1b38713323f17da539ace839b
Lorenzzo Pagotti <lzpagotti@gmail.com>
2026-04-13 14:43:25 -0300

    Update heap.c

--- heap.c
+++ heap.c
@@ -126,54 +126,61 @@ void alterarPrioridade(Heap* h, int codigo_triagem, int nova_prioridade)
     printf("Fila reoordenada.\n");
 }
 
-void imprimirFila(Heap* h) {
-    printf("\n--- FILA DE ESPERA (Ordem do Array) ---\n");
-    for (int i = 0; i < h->tamanho; i++) {
-        printf("Prioridade %d | Triagem: %d | Nome: %s\n", 
-               h->pacientes[i].prioridade, h->pacientes[i].codigo_triagem, h->pacientes[i].nome);
+void imprimirFila(Heap* h) 
+{
+    printf("\n--- FILA DE ESPERA ---\n");
+    for (int i = 0; i < h->tamanho; i++) 
+    {
+        printf("Prioridade %d | Triagem: %d | Nome: %s\n", h->pacientes[i].prioridade, h->pacientes[i].codigo_triagem, h->pacientes[i].nome);
     }
 }
 
-// Ajudante recursiva que desenha os galhos conectando os nós
 void imprimirArvoreRecursivo(Heap* h, int indice, int tracos[], int nivel, int isFilhoDireito) {
     if (indice >= h->tamanho) return;
 
-    // 1. Vai pro filho da direita primeiro (parte de cima da tela)
-    if (2 * indice + 2 < h->tamanho) {
-        tracos[nivel] = isFilhoDireito ? 0 : 1; // Decide se precisa descer uma linha reta '|'
+    if (2 * indice + 2 < h->tamanho) 
+    {
+        tracos[nivel] = isFilhoDireito ? 0 : 1;
         imprimirArvoreRecursivo(h, 2 * indice + 2, tracos, nivel + 1, 1);
     }
 
-    // 2. Desenha a estrutura de galhos antes do nó
-    for (int i = 0; i < nivel - 1; i++) {
+    for (int i = 0; i < nivel - 1; i++) 
+    {
         printf(tracos[i] ? "│   " : "    ");
     }
-    if (nivel > 0) {
+    if (nivel > 0) 
+    {
         printf(isFilhoDireito ? "┌── " : "└── ");
-    } else {
-        printf("➤ "); // Indicador visual para a Raiz
+    } 
+    else 
+    {
+        printf("➤ ");
     }
 
-    // 3. Imprime o paciente com as cores da gravidade
     Paciente p = h->pacientes[indice];
-    if (p.prioridade == 1) {
+    if (p.prioridade == 1) 
+    {
         printf(COR_VERMELHA);
-    } else if (p.prioridade <= 3) {
+    } 
+    else if (p.prioridade <= 3) 
+    {
         printf(COR_AMARELA);
-    } else {
+    } 
+    else 
+    {
         printf(COR_VERDE);
     }
     printf("[P%d] %d (%s)" COR_RESET "\n", p.prioridade, p.codigo_triagem, p.nome);
 
-    // 4. Vai pro filho da esquerda (parte de baixo da tela)
-    if (2 * indice + 1 < h->tamanho) {
+    if (2 * indice + 1 < h->tamanho) 
+    {
         tracos[nivel] = isFilhoDireito ? 1 : 0;
         imprimirArvoreRecursivo(h, 2 * indice + 1, tracos, nivel + 1, 0);
     }
 }
 
-// Função principal que o main chama
-void imprimirArvoreASCII(Heap* h) {
-    int tracos[100] = {0}; // Array que aguenta árvores gigantes (100 níveis de profundidade)
+void imprimirArvoreASCII(Heap* h) 
+{
+    int tracos[100] = {0};
     imprimirArvoreRecursivo(h, 0, tracos, 0, 0);
 }
